0c6ebafbeccc458421588be34ca0e7ba91cd5260
thanujasiva26-cmyk <thanujasiva26@gmail.com>
2026-04-07 00:02:26 +0530

    Added Customer admin configuration and authentication views
    -Configured Customer model in Django admin panel
    -Added role-based fields (admin ,staff ,member)
    -Created login and logout views in auth_views.py
    -Integrated authentication system for library management system

---
+++ admin.py/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password', 'role')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'role', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email', 'username')
+    ordering = ('username',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
